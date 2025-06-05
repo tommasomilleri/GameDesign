5f54bac4128164ad58181e1339adb5728aa927d0
Lukap04 <polloniluka@gmail.com>
2025-06-05 18:21:11 +0200

    Spawn delle carte a inizio partita
    Ora, all'inizio della partita spawnano dieci carte (ovviamente il numero è intercambiabile a piacere) in posizioni casuali. Ancora non è possibile interagire con queste carte.

---
+++ GameDesign/Assets/Scripts/Empty_Card.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Empty_Card : MonoBehaviour
+{
+    public int my_value;
+
+    public string my_suit;
+}
--- GameDesign/Assets/Scripts/GenerateCards.cs
+++ GameDesign/Assets/Scripts/GenerateCards.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -13,16 +14,71 @@ public class GenerateCards : MonoBehaviour
         (1, 'd'), (2, 'd'), (3, 'd'), (4,'d'), (5, 'd'), (6, 'd'), (7, 'd')
     };
 
+    public GameObject card_to_copy;
+
+    public Material coppe;
+    public Material spade;
+    public Material denari;
+    public Material bastoni;
+
     void Awake()
     {
+        card_to_copy.SetActive(false);
         List<(int, char)> cards2 = new List<(int, char)>();
         foreach ((int, char) card in cards)
         {
             cards2.Add(card);
         }
-        /* for (int i = 0; i < 15; i++)
+        for (int i = 0; i < 10; i++)
         {
-            //Spawn a card
-        } */
+            CreateCard(cards2);
+        }
     }
+
+
+    public void SpawnCard(GameObject c) //fa apparire la carta passata sul terreno di gioco
+    {
+        //da inserire il controllo sulle due fasi di gioco
+        float x = UnityEngine.Random.Range(-20.000000f, 20.000000f);
+        float z = UnityEngine.Random.Range(-2.000000f, 15.000000f);
+
+        c.transform.position = new Vector3(x, (float)3.481633, z);
+        c.SetActive(true);
+        return;
+    }
+
+    public void CreateCard(List<(int, char)> deck)
+    {
+        GameObject my_card = Instantiate(card_to_copy);
+        my_card.SetActive(false);
+
+        System.Random rnd = new System.Random();
+        int n = rnd.Next(0, deck.Count);
+        my_card.GetComponent<Empty_Card>().my_value = deck[n].Item1;
+        switch (deck[n].Item2)
+        {
+            case 'c':
+                my_card.GetComponent<Empty_Card>().my_suit = "coppe";
+                my_card.GetComponent<MeshRenderer>().material = coppe;
+                break;
+            case 'd':
+                my_card.GetComponent<Empty_Card>().my_suit = "denari";
+                my_card.GetComponent<MeshRenderer>().material = denari;
+                break;
+            case 'b':
+                my_card.GetComponent<Empty_Card>().my_suit = "bastoni";
+                my_card.GetComponent<MeshRenderer>().material = bastoni;
+                break;
+            case 's':
+                my_card.GetComponent<Empty_Card>().my_suit = "spade";
+                my_card.GetComponent<MeshRenderer>().material = spade;
+                break;
+            default:
+                Environment.Exit(0);
+                break;
+        }
+        SpawnCard(my_card);
+        deck.RemoveAt(n);
+    }
+    /*servono un metodo Spawn e un metodo che crea un oggetto e gli dà tutti gli attributi della carta*/
 }
