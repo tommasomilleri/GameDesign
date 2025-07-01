69b846dc0f2031ab05eb8108fb7c646e8224e1a8
Mastergame-20 <alessandro.yuma02@gmail.com>
2025-07-01 15:22:45 +0200

    stats update

--- GameDesign/Assets/Scripts/Stats.cs
+++ GameDesign/Assets/Scripts/Stats.cs
@@ -3,9 +3,9 @@ using UnityEngine;
 
 public static class Stats_Fante
 {
-    public static int START_LIFE = 200;
-    public static int START_ATTACK = 16;
-    public static int START_DEFENSE = 15;
+    public static int START_LIFE = 45;
+    public static int START_ATTACK = 6;
+    public static int START_DEFENSE = 4;
     public static float START_MOVEMENT_SPEED = 8.5f;
     public static int START_MONEY = 0;
     public static float START_RANGE = 2.5f;
@@ -15,9 +15,9 @@ public static class Stats_Fante
 
 public static class Stats_Cavallo
 {
-    public static int START_LIFE = 150;
-    public static int START_ATTACK = 25;
-    public static int START_DEFENSE = 8;
+    public static int START_LIFE = 30;
+    public static int START_ATTACK = 10;
+    public static int START_DEFENSE = 2;
     public static float START_MOVEMENT_SPEED = 10.5f;
     public static int START_MONEY = 3;
     public static float START_RANGE = 2f;
@@ -27,9 +27,9 @@ public static class Stats_Cavallo
 
 public static class Stats_Re
 {
-    public static int START_LIFE = 250;
-    public static int START_ATTACK = 30;
-    public static int START_DEFENSE = 15;
+    public static int START_LIFE = 60;
+    public static int START_ATTACK = 7;
+    public static int START_DEFENSE = 5;
     public static float START_MOVEMENT_SPEED = 6.5f;
     public static int START_MONEY = 5;
     public static float START_RANGE = 2f;
